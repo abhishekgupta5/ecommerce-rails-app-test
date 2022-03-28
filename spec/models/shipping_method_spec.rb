ff4b512c296f9ab97d26738a522727c0057c5b33
Abhishek Gupta <abhishekmodi2011@gmail.com>
2022-03-28 22:45:22 +0530

    Added functionality to manage shipping methods
    Any admin should be able to add shipping methods,
    add for multiple countries or delete for a country
    via rails console

--- spec/models/shipping_method_spec.rb
+++ spec/models/shipping_method_spec.rb
@@ -29,4 +29,48 @@ RSpec.describe ShippingMethod, type: :model do
     create(:shipping_method, country: :IN)
     expect(build_stubbed(:shipping_method, country: :IN)).to_not be_valid
   end
+
+  describe "#add" do
+    it "creates shipping method with valid attributes" do
+      expect do
+        ShippingMethod.add(shipping_method_valid_attributes)
+      end.to change { ShippingMethod.count }.by(1)
+    end
+  end
+
+  describe "#add_for_countries" do
+    it "creates shipping method with valid attributes" do
+      expect do
+        ShippingMethod.add_for_countries("fedex", countries_to_delivery_time_map)
+      end.to change { ShippingMethod.count }.by(3)
+      # Check data
+      expect(ShippingMethod.where(country: countries_to_delivery_time_map.keys)
+                           .pluck(:name, :country, :delivery_time_in_days))
+        .to match_array(expected_response_add_for_countries)
+    end
+  end
+
+  describe "#delete_for_country" do
+    it "deletes shipping method for a country" do
+      record = create(:shipping_method, country: "IN")
+      expect do
+        ShippingMethod.delete_for_country("IN")
+      end.to change { ShippingMethod.count }.by(-1)
+      expect { record.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  private
+
+  def shipping_method_valid_attributes
+    {name: "fedex", country: "IN", delivery_time_in_days: 10}
+  end
+
+  def countries_to_delivery_time_map
+    {IN: 5, US: 2, IT: 3}
+  end
+
+  def expected_response_add_for_countries
+    [["fedex", "IN", 5], ["fedex", "US", 2], ["fedex", "IT", 3]]
+  end
 end
