cceaddd7cbeb14259ecd1f3684a9bc710fc5f686
Abhishek Gupta <abhishekmodi2011@gmail.com>
2022-03-29 21:02:10 +0530

    Added unit tests for new methods in ShippingMethod and Order models

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -23,4 +23,24 @@ RSpec.describe Order do
       expect(order.total).to eq(35.0)
     end
   end
+
+  describe "#shipping_info_for_order" do
+    it "returns value when shipping method is present" do
+      address = build_stubbed(:address)
+      shipping_method = build_stubbed(:shipping_method, country: address.country)
+      build_stubbed(:order, shipping_address: address) do |o|
+        allow(o).to receive(:shipping_info_for_order).and_return({shipping_method_name: shipping_method.name,
+                                                                  delivery_time: shipping_method.delivery_time_in_days})
+      end
+    end
+
+    it "returns nil values when shipping method is absent" do
+      address = build_stubbed(:address)
+      ShippingMethod.where(country: address.country).delete_all
+      build_stubbed(:order, shipping_address: address) do |o|
+        allow(o).to receive(:shipping_info_for_order).and_return({shipping_method_name: nil,
+                                                                  delivery_time: nil})
+      end
+    end
+  end
 end
