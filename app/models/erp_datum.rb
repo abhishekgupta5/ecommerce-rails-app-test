851d6674c297378e3e46f758b2c9a471084d2ca8
Abhishek Gupta <abhishekmodi2011@gmail.com>
2022-03-30 03:36:04 +0530

    Added glue code to fetch and update erp data
    associate_erp_and_order will fetch and update erp data for
    any order. Currently this is possible to do via rails console.

--- app/models/erp_datum.rb
+++ app/models/erp_datum.rb
@@ -4,4 +4,21 @@ class ErpDatum < ApplicationRecord
 
   validates :entity_id, presence: true
   validates :order_id, presence: true
+
+  class << self
+    def update_erp_data(order)
+      entity_id = fetch(order)
+      save_in_db(entity_id, order)
+    end
+
+    private
+
+    def fetch(order)
+      ErpClient.new(order).post["id"]
+    end
+
+    def save_in_db(entity_id, order)
+      ErpDatum.create!(entity_id: entity_id, order: order)
+    end
+  end
 end
